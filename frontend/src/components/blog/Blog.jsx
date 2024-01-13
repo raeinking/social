87a6e4885011f87410a174cdbf6767e79cc0cea5
Mrcode93 <alhlaqalhlaq667@gmail.com>
2024-01-13 06:18:02 +0300

    v.3-by-amer-at6:17am

--- frontend/src/components/blog/Blog.jsx
+++ frontend/src/components/blog/Blog.jsx
@@ -1,12 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../blog/blog.css";
+// import path from "path";
+
 function Blog({ name }) {
+  const [posts, setPosts] = useState([]);
+  const [fileName, setFileName] = useState("");
+
+  useEffect(() => {
+    // fetch data from server
+    fetch("http://localhost:8040/post/getPosts", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then((res) => {
+      res.json().then((data) => {
+        console.log(data);
+        setPosts(data);
+      });
+    });
+    getResult();
+  }, []);
+
+  const getResult = () => {
+    posts.forEach((post) => {
+      console.log(post.video);
+      // Extract filename from the file path
+      // const filename = path.basename(post.video);
+      const filename = post.video.split("/").pop();
+      setFileName(filename);
+    });
+  };
+
   return (
     <div className="blogcontainer">
-      <div className="header">
-        <h1>{name}</h1>
-        <div className="avatar">H</div>
-      </div>
+      {" "}
+      {posts.map((post) => (
+        //// const filename = path.basename(post.video);
+
+        <div className="blog" key={post._id}>
+          <h1> {post.title} </h1>{" "}
+          <video
+            src={`http://localhost:8040/api/video/${fileName}`}
+            controls
+            width="500"
+            height="auto"
+          />
+        </div>
+      ))}{" "}
     </div>
   );
 }
